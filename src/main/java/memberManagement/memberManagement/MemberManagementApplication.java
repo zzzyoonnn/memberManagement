b57b76021f378ab57ae591026b6f431c1dece58b
zzzyoonnn <dwsrh03@gmail.com>
2024-05-26 01:47:43 +0900

    test

---
+++ src/main/java/memberManagement/memberManagement/MemberManagementApplication.java
@@ -0,0 +1,13 @@
+package memberManagement.memberManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/memberManagement/memberManagement/MemberManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package memberManagement.memberManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
