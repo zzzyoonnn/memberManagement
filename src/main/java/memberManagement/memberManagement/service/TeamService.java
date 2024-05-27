b2ed5d7eb15e416fd7c2f0e388a164bc24395d35
zzzyoonnn <dwsrh03@gmail.com>
2024-05-27 12:18:55 +0900

    미니 프로젝트

--- src/main/java/memberManagement/memberManagement/service/TeamService.java
+++ src/main/java/memberManagement/memberManagement/service/TeamService.java
@@ -2,9 +2,11 @@ package memberManagement.memberManagement.service;
 
 import memberManagement.memberManagement.domain.Team;
 import memberManagement.memberManagement.repository.TeamRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TeamService {
 
     private final TeamRepository teamRepository;
